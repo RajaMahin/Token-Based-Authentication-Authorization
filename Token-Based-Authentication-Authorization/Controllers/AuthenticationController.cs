56adb62a54da5f45c650cc927472a0fecaf47fec
Raja Mahin Zaman <62209180+RajaMahin@users.noreply.github.com>
2025-07-20 05:55:21 +0500

    Setting Up Authentication Controller

---
+++ Token-Based-Authentication-Authorization/Controllers/AuthenticationController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Token_Based_Authentication_Authorization.Data;
+using Token_Based_Authentication_Authorization.Data.Models;
+
+namespace Token_Based_Authentication_Authorization.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly AppDbContext _context;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationController(
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            AppDbContext context,
+            IConfiguration configuration
+
+            )
+        {
+
+
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _context = context;
+            _configuration = configuration;
+        }
+    }
+}
