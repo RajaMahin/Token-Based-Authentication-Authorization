952125de722f2e318b596560eaa8ef6ba4f280f5
Raja Mahin Zaman <62209180+RajaMahin@users.noreply.github.com>
2025-07-23 10:31:49 +0500

    Adding Role Authentication to a controller

--- Token-Based-Authentication-Authorization/Controllers/ManagementController.cs
+++ Token-Based-Authentication-Authorization/Controllers/ManagementController.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Token_Based_Authentication_Authorization.Data.Helpers;
 
 namespace SchoolApp.API.Controllers
 {
     [ApiController]
+    [Authorize(Roles = UserRoles.Manager)]
     [Route("[controller]")]
     public class ManagementController : ControllerBase
     {
